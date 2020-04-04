695d601f27199b6c179265806d0ff733b9219e36
comverser <hyunduk0206@gmail.com>
2020-04-04 23:49:47 +0900

    Add files via upload

---
+++ smhrd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[263]:
+
+
+def wf(x):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    plt.plot(x)
+    plt.grid()
+    plt.xlim(0,10)
+    plt.ylim(-10,10)
+    plt.xticks(np.arange(0,11,1))
+    plt.yticks(np.arange(-10,11,2))
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+
